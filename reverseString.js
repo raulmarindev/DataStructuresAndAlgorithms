1cf5bc6dcba04aa7d372e34b3f04f39ea919040e
Raúl Marín <rmarin82@gmail.com>
2020-04-02 20:17:00 +0200

    Add implementation of reverse string algorithm.

---
+++ reverseString.js
@@ -0,0 +1,28 @@
+"use strict";
+// Create a function that reverses a string:
+// 'Hi My name is Andrei' should be:
+// 'ierdnA si eman iH'
+// Questions
+// Q1 Will the parameter allways be a string?
+// Q2 Is there a limit to the length of the string?
+// O(n)
+const reverseString = (sourceString) => {
+    let reversedString = '';
+    for (let i = sourceString.length - 1; i >= 0; i--) {
+        reversedString += sourceString[i];
+    }
+    return reversedString;
+};
+const reverseString2 = (sourceString) => {
+    return [...sourceString].reverse().join('');
+};
+const reverseString3 = (sourceString) => {
+    let reversedString = [];
+    for (let i = sourceString.length - 1; i >= 0; i--) {
+        reversedString.push(sourceString[i]);
+    }
+    return reversedString.join('');
+};
+console.log(reverseString('Hi My name is Andrei'));
+console.log(reverseString2('Hi My name is Andrei'));
+console.log(reverseString3('Hi My name is Andrei'));
